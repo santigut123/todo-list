4a8d9c2d5c9d532be1403fda0758a78d617c8312
Santi Gutierrez <santigut@posteo.net>
2021-05-28 17:42:16 -0400

    Made headers, linked stylesheet

--- src/divs.js
+++ src/divs.js
@@ -1,13 +1,31 @@
 function setId(item,id){
   item.setAttribute("id",id);
 }
-
-function makePage(content){
-  console.log("YAA");
-
-  let header=document.createElement("div");
+function setContents(item,contents){
+  item.textContent=contents;
+}
+function setClass(item,theClass){
+  item.setAttribute("class",theClass);
+}
+function fillHeader(header){
   setId(header,"header");
+  let todoHeader = document.createElement("div");
+  let contentHeader = document.createElement("div");
+  setId(todoHeader, "todoHeader")
+  setId(contentHeader,"contentHeader");
+  setContents(todoHeader,"TODOS");
+  setContents(contentHeader,"CONTENTS");
+  setClass(todoHeader,"headers");
+  setClass(contentHeader,"headers");
+  header.appendChild(todoHeader);
+  header.appendChild(contentHeader);
+
+}
+function makePage(content){
+  let header=document.createElement("div");
+  fillHeader(header);
   content.appendChild(header);
+
 
 
 
